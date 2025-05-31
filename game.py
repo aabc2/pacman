b0f7b58582b86fff2b4197850230b49acff39e31
aabc2 <aabc2@alu.ua.es>
2025-05-31 20:09:15 +0200

    semilla 13 juego 68 y 69 victoria

--- game.py
+++ game.py
@@ -35,7 +35,7 @@ import sys
 ###################################################
 import gamedata # Para la extraccion de datos de la partida
 import random
-random.seed(12) # Para reproducibilidad de los resultados
+random.seed(13) # Para reproducibilidad de los resultados
 ###################################################
 
 class Agent:
--- net.py
+++ net.py
@@ -13,8 +13,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 # Fijamos todas las semillas para reproducibilidad
-torch.manual_seed(12)
-random.seed(12)
+torch.manual_seed(13)
+random.seed(13)
 # Constantes
 INPUT_SIZE = None  # Se determinará en tiempo de ejecución basado en el tamaño del mapa
 HIDDEN_SIZE = 128
